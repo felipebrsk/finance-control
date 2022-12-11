871ce2f297333bcfb5426d727e9d311d4c7e20c4
felipebrsk <felipeoliveira@triventoeducacao.com.br>
2022-12-10 22:33:02 -0300

    tests: adding helpers tests

---
+++ tests/Unit/Helpers/MoneyHelpersTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use Tests\TestCase;
+
+class MoneyHelpersTest extends TestCase
+{
+    /**
+     * Setup new test environments.
+     * 
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Test if can format the given amount to given currency.
+     * 
+     * @return void
+     */
+    public function test_if_can_format_the_given_amount_to_given_currency(): void
+    {
+        $amount = 10000;
+
+        $brlAmount = formatCurrency($amount, 'BRL');
+
+        $this->assertTrue(
+            $amount !== $brlAmount &&
+                $brlAmount === 'R$ 100,00'
+        );
+
+        $usdAmount = formatCurrency($amount, 'USD');
+
+        $this->assertTrue(
+            $amount !== $usdAmount &&
+                $usdAmount === 'US$ 100,00'
+        );
+    }
+}
